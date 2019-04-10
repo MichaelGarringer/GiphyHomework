0b75625f4670cfed55d848b5789aeb0e75f98ce0
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-04-10 18:05:54 -0500

    ajax function not working properly: $ not defined???

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,10 @@
-queryURL = "giphy + //apikey 4YH971Skk7dE3erBkplFkZ4bMnUHdza7"
+
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=4YH971Skk7dE3erBkplFkZ4bMnUHdza7&tag=&rating=G";
 console.log(queryURL)
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response.type);
+      });
