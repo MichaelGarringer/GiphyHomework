c7d210bbb2980691c2e27ab14574c75afbea9b40
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-04-12 14:19:01 -0500

    full functionality of app new buttons added and search function functional now just need to pause and unpause gifs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,44 @@
+var buttons = ["bear", "dog", "cat", "duck"]
 
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=4YH971Skk7dE3erBkplFkZ4bMnUHdza7&tag=&rating=G";
-console.log(queryURL)
 
+
+//Loop through buttons array and create buttons
+function displayButtons(){
+    $("#buttons-container").empty()
+    for (var i = 0; i < buttons.length; i++) {
+        var newButton = $("<button> </button>")
+        newButton.text(buttons[i])
+        newButton.addClass("each-button")
+        $("#buttons-container").append(newButton)
+    
+    };
+}
+
+displayButtons()
+
+//Click Event for buttons
+$(document).on("click", ".each-button", function (event) {
+    var searchTerm = $(this).text()
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=4YH971Skk7dE3erBkplFkZ4bMnUHdza7&limit=10`;
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
-        console.log(response.type);
-      });
+    }).done(function (response) {
+        var responsePlusData = response.data
+        for (var i = 0; i < responsePlusData.length; i++) {
+            $("#gifDiv").prepend("<img src='" + responsePlusData[i].images.fixed_height.url + "'>");
+        }
+    });
+});
+
+//Submit even from search form
+$("#search").on("submit", function(event){
+    event.preventDefault()
+    var newButton = $("#userInput").val().trim();
+    console.log(newButton)
+
+    buttons.push(newButton)
+    displayButtons()
+
+});
+
